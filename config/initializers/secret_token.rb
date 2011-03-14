54489a0d2183973b9e0170f2c114aee56e83672c
Sami Saada <sami.saada@gmail.com>
2011-03-14 15:35:14 +0200

    initial commit with rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asiakirjajakelu::Application.config.secret_token = '1e963b78d1892971ca7a2178954f606bc6208963183449a0108920ea708dbe267da3b2801fe2f3762c13d0f8a947ef05a243222e7d163682228d5dd704e9cb28'
